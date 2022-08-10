bf3d76efd3d2b80d2ac3ce378676439ee717d430
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-08-10 16:51:37 +0530

    Add files via upload

--- 135.py
+++ 135.py
@@ -1,6 +1,5 @@
 from turtle import distance
 import pandas as pd
-import matplotlib as plt
 import plotly.express as px
 import csv
 
@@ -8,16 +7,16 @@ f=pd.read_csv('a.csv')
 mass=f['Mass'].to_list()
 radius =f['Radius'].to_list()
 d =f['Distance'].to_list()
-g =f['Unnamed'].to_list()
+g =f['Unnamed: 0'].to_list()
 name =f['Star_name'].to_list()
-fig=px.scatter(x=name,y=d)
+fig=px.scatter(y=name,x=d)
 fig.show()
 
-fig1=px.scatter(x=name,y=mass)
+fig1=px.scatter(y=name,x=mass)
 fig1.show()
 
-fig2=px.scatter(x=name,y=g)
+fig2=px.scatter(y=name,x=g)
 fig2.show()
 
-fig3=px.scatter(x=name,y=radius)
+fig3=px.scatter(y=name,x=radius)
 fig3.show()
