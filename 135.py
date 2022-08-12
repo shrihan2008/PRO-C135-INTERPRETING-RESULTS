44472f60d9f5087f51f6f013226d82de3d73116c
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-08-12 22:32:55 +0530

    Add files via upload

--- 135.py
+++ 135.py
@@ -1,22 +1,15 @@
-from turtle import distance
-import pandas as pd
-import plotly.express as px
-import csv
 
-f=pd.read_csv('a.csv')
+import pandas as pd
+import csv
+import matplotlib.pyplot as plt
+f=pd.read_csv('star_with_gravity.csv')
 mass=f['Mass'].to_list()
 radius =f['Radius'].to_list()
 d =f['Distance'].to_list()
-g =f['Unnamed: 0'].to_list()
+g =f['Gravity'].to_list()
 name =f['Star_name'].to_list()
-fig=px.bar(x=name,y=d)
-fig.show()
 
-fig1=px.bar(x=name,y=mass)
-fig1.show()
-
-fig2=px.bar(x=name,y=g)
-fig2.show()
-
-fig3=px.bar(x=name,y=radius)
-fig3.show()
+plt.plot(name, d)
+plt.plot(name, g)
+plt.plot(name, radius)
+plt.plot(name, mass)
